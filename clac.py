57fc8fcec59b8b5bda8bc500a56345d680219ed0
Tom-Wang77 <1833971291@qq.com>
2026-06-28 13:01:02 +0800

    day5

---
+++ clac.py
@@ -0,0 +1,15 @@
+a_str = input("Input the first numb.")
+b_str = input("Input the second numb.")
+a = int(a_str)
+b = int(b_str)
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+div_result = a / b
+print ("=" * 30)
+print (f"{a}+{b}={sum_result}")
+print (f"{a}-{b}={diff_result}")
+print (f"{a}*{b}={prod_result}")
+print (f"{a}/{b}={div_result}")
+print("=" * 30)
+
